42060edd35e73d2a4f5470c6ad3ec265597ec7f6
Edward De Faria <edward.de-faria@ovh.net>
2018-10-02 19:00:16 +0200

    Lower priority error "Cannot convert timestamp"

--- lib/logstash/codecs/gelf.rb
+++ lib/logstash/codecs/gelf.rb
@@ -224,7 +224,7 @@ class LogStash::Codecs::Gelf < LogStash::Codecs::Base
           begin
             dt = DateTime.parse(event.get("@timestamp").to_iso8601).to_time.to_f
           rescue StandardError
-            @logger.warn("Cannot convert @timestamp", :timestamp => event.get("@timestamp"))
+            @logger.debug("Cannot convert @timestamp", :timestamp => event.get("@timestamp"))
             dt = DateTime.now.to_time.to_f
           end
           event.set("timestamp", dt)
@@ -236,7 +236,7 @@ class LogStash::Codecs::Gelf < LogStash::Codecs::Base
           begin
             dt = DateTime.parse(event.get("timestamp").to_iso8601).to_time.to_f
           rescue StandardError
-            @logger.warn("Cannot convert timestamp", :timestamp => event.get("timestamp"))
+            @logger.debug("Cannot convert timestamp", :timestamp => event.get("timestamp"))
             dt = DateTime.now.to_time.to_f
           end
         end
